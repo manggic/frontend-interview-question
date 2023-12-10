72ea9b3a8ed30ff849d66ea79ab69d0843ae372b
manish mahto <manish.mahto@techno-soft.in>
2023-12-10 17:21:07 +0530

    added react interview Question pdf

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+document.querySelector(".react").addEventListener("click", function () {
+  document.querySelector(".pdf-viewer").style.display = "block";
+  document.querySelector(".content-heading").style.display = "none";
+  document.querySelector("#pdf-container").style.paddingTop = "0px";
+});
